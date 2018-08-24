d85a66bc03c0f0467cb744f67b4880fa397356b0
SanSun <sandy.sundram@gmail.com>
2018-08-23 20:44:40 -0700

    db created

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,13 +1,31 @@
-DROP DATABASE IF EXISTS test_db;
+DROP DATABASE IF EXISTS codebook_db;
 
-CREATE DATABASE test_db;
+CREATE DATABASE codebook_db;
 
-USE test_db;
+USE codebook_db;
 
-CREATE TABLE items (
+CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
-  item VARCHAR(255),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email_address VARCHAR (255),
+  username VARCHAR (255),
+  password_hash VARCHAR(255),
+  password_salt VARCHAR (255),
+  gitlink VARCHAR (255),
+  linknin VARCHAR(255),
+  photourl VARCHAR (255),
   PRIMARY KEY (id)
 );
 
-INSERT INTO items (item) VALUES ('item1'),('item2'),('item3'),('item4');
+CREATE TABLE gitrepos (
+  id INT NOT NULL AUTO_INCREMENT,
+  git_repo VARCHAR(255),
+  user_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+
+INSERT INTO users SET ("Kit","Te","kjonthante@gmail.com","kit","kit","kit","gitlink","linkdinlink","photo");
