c0dd77ae7c13541cb3bbbe4fd5afc358f386b4d3
Kit Jonathan Te <kjonathante@gmail.com>
2018-08-25 17:03:29 -0700

    add app/routes/routes.js for modular design of routes

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 var express = require('express')
-var mysql = require('mysql')
 
-//var config = require('./app/conf/conf')
 var db = require('./app/db/db.js')
+var routes = require('./app/routes/routes.js')
 
 var app = express()
 
 db.connect()
 
-app.get('/', function(req, res) {
-  res.send('Hello')
-})
+app.use('/', routes)
 
-app.get('/db', function(req, res) {
+
+
+// app.get('/db', function(req, res) {
   
-  db.pool().query({
-    sql: 'SELECT * FROM users'
-  }, function(error, results, fields){
-    if (error) throw error
+//   db.pool().query({
+//     sql: 'SELECT * FROM users'
+//   }, function(error, results, fields){
+//     if (error) throw error
 
-    var str = ''
-    for(var val of results) {
-      str += val.id + '</br>'
-    }
-    res.send(str)
-  })
-})
+//     var str = ''
+//     for(var val of results) {
+//       str += val.id + '</br>'
+//     }
+//     res.send(str)
+//   })
+// })
 
 app.listen(3000)
