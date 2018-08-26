5a6d6379fdf41f01680eb3e794084132160c5ff6
Taylor Aanenson <tayloraanenson@Taylors-MacBook-Pro.local>
2018-08-26 01:43:53 -0700

    recreated files after deletion

---
+++ public/assets/javascript/signup.js
@@ -0,0 +1,41 @@
+// Hides load gif after page load
+$(document).ready(function(){
+    $("#load-gif").fadeOut("slow");
+});
+
+// Validates sign up form
+(function formValidation(){
+    'use strict';
+    window.addEventListener('load', function() {
+    var forms = document.getElementsByClassName('needs-validation');
+    Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            var first = $('#validationCustom01').val();
+            var last = $('#validationCustom02').val();
+            var user = $('#validationCustomUsername').val();
+            var email = $('#validationCustom03').val();
+            var pass = $('#inputPassword').val();
+            var conf = $('#confirmPassword').val();
+            // console.log(first,last,user,email,pass,conf);
+        if (email.indexOf('@') === -1 || email.indexOf('.com') === -1){
+            event.preventDefault();
+            event.stopPropagation();
+            $('#validationCustom03').val('');
+        }
+        if (pass !== conf){
+            event.preventDefault();
+            event.stopPropagation();
+            $('#inputPassword').val('');
+            $('#confirmPassword').val('');
+            $('#pass .invalid-feedback').text('Great Scott!');
+            $('#conf .invalid-feedback').text('Your passwords don\'t match!')
+        }
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+        }, false);
+    });
+    }, false);
+})();
