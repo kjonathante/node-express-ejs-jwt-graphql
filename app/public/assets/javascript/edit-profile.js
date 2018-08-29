6c153b1f766f25cdb094e7e0f6dc6298dbfd52a0
Taylor Aanenson <tayloraanenson@Taylors-MacBook-Pro.local>
2018-08-29 16:39:13 -0700

    added repo selection and updated responsiveness

--- app/public/assets/javascript/edit-profile.js
+++ app/public/assets/javascript/edit-profile.js
@@ -11,3 +11,54 @@ function readURL(input) {
 $("#pic-upload").change(function(){
     readURL(this);
 });
+
+function gitPull(){
+    event.preventDefault();
+
+    var username = $('#validationCustom04').val();
+
+    console.log(username);
+  
+    $.ajax({
+      url: '/github/repos/'+username
+    }).then(function(data, textStatus, errorThrown){
+        var res = data.data;
+        $('#validationCustom04').val('');
+        $('#repo-btn').attr('class', 'btn btn-success');
+        $('#repo-btn').text('Success!');
+        $('#validationCustom04').val('Please select the repos you would like to show off');
+        $('#github-selection').show();
+        for (var i in res){
+            var repoNames = res[i].name;
+            var urls = res[i].url;
+            var div = $('<div>');
+            div.attr('class', 'form-check');
+            var input = $('<input>');
+            input.attr('class', 'form-check-input');
+            input.attr('type', 'checkbox');
+            input.attr('name', 'git_repo');
+            input.attr('value', urls);
+            var label = $('<label>');
+            label.attr('class', 'form-check-label');
+            label.text(repoNames);
+            div.append(input,label)
+            console.log(res[i]);
+            $('#github-selection').append(div);
+        }
+      },
+      function(jqXHR, textStatus, errorThrown){
+        console.log(textStatus, errorThrown)
+        if (errorThrown === 'Bad Request'){
+            $('#validationCustom04').val('');
+            $('#repo-btn').attr('class', 'btn btn-danger');
+            $('#repo-btn').text('Error..');
+            $('#validationCustom04').attr('placeholder', 'That username does not exist');
+        }else if(errorThrown === 'Not Found'){
+            $('#validationCustom04').val('');
+            $('#repo-btn').text('Error..');
+            $('#repo-btn').attr('class', 'btn btn-danger');
+            $('#validationCustom04').attr('placeholder', 'Please enter your username');
+        }
+      }
+    );
+}
