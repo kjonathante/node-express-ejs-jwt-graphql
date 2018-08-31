3e83723c9471c6f2b14a747e3a7083670fdec43a
Kit Jonathan Te <kjonathante@gmail.com>
2018-08-31 12:52:40 -0700

    modify editProfile and editProfilePage. create gitrepo.model.js, insert and findByUserId functions. modify the schema.db

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,14 +1,17 @@
 //--------------------------------------------------------EXPORTS & REQUIRES---------------------------------------
+// native
+var path = require('path')
+// npm
 // including bcrypt for password encryption
 var bcrypt = require('bcryptjs');
 var nodemailer = require('nodemailer');
-
+// local
 // exporting user.model.js which runs all the DB functions.
 var user = require('../models/user.model.js')
+var gitrepo = require('../models/gitrepo.model.js')
 var db = require('../db/db.js')
-var config = require('../conf/config.js');
-var path = require('path')
 
+var config = require('../conf/config.js');
 email = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -155,38 +158,90 @@ exports.authorize = function( req, res, next ) {
 }
 
 exports.editProfilePage = function (req, res, next){
-  user.findById( req.session.user.userInfo.id, function(error,results) {
+  var id = req.session.user.userInfo.id
+  user.findById( id, function(error,userInfo) {
     if (error) {
       return next(error)
     } else {
-      return res.render('pages/edit-profile', { userInfo: results } );
+      gitrepo.findByUserId(id, function(error,userGitRepos){
+        if (error) {
+          return next(error)
+        } else {
+          console.log('Inside editProfilePage -> userGitRepos ', userGitRepos)
+          userInfo.git_repo = userGitRepos
+    
+          console.log('Inside editProfilePage -> userInfo ', userInfo)
+          return res.render('pages/edit-profile', {userInfo: userInfo} );    
+        }
+      })
     }
   })
 }
 
-exports.editProfile = function (req, res){
-  console.log(req.body,req.files);
-  if (!req.files){
-    return res.status(400).send('No files were uploaded.');
-  }
-  let ext = req.files.photourl.name.split('.');
-  console.log(ext[1]);
+exports.editProfile = function (req, res, next){
+  console.log('Inside editProfile -->> req.body',req.body)
+  console.log('Inside editProfile -->> req.files',req.files);
 
-  let photoFile = req.files.photourl;
-  // console.log('PHOTOFILE: '+req.files.photourl);
-  let fileName = req.session.user.userInfo.id+"_"+req.session.user.userInfo.first_name+"_"+req.session.user.userInfo.last_name+"."+ext[1];
-  photoFile.mv(path.join(__dirname,'../public/images/')+fileName, function(err) {
-    if (err)
-      return res.status(500).send(err);
+
+  // if (!req.files){
+  //   return res.status(400).send('No files were uploaded.');
+  // }
+  // let ext = req.files.photourl.name.split('.');
+  // console.log(ext[1]);
+
+  // let photoFile = req.files.photourl;
+  // let fileName = req.session.user.userInfo.id+"_"+req.session.user.userInfo.first_name+"_"+req.session.user.userInfo.last_name+"."+ext[1];
+  // photoFile.mv(path.join(__dirname,'../public/images/')+fileName, function(err) {
+  //   if (err)
+  //     return res.status(500).send(err);
  
-    res.send('File uploaded!');
-  });
-  db.pool().query('UPDATE users SET gitlink = ?, linkdin = ?, photourl = ? WHERE id = ?',
-    [req.body.gitlink,req.body.linkdin,fileName], function (err,results,fields){
-      if (err){
-        console.log(err);
-      }else{
-        res.render('pages/profile',req.session.user);
+  //   res.send('File uploaded!');
+  // });
+  // db.pool().query('UPDATE users SET gitlink = ?, linkdin = ?, photourl = ? WHERE id = ?',
+  //   [req.body.gitlink,req.body.linkdin,fileName], function (err,results,fields){
+  //     if (err){
+  //       console.log(err);
+  //     }else{
+  //       res.render('pages/profile',req.session.user);
+  //     }
+  // })
+
+  var fileName = ""
+  var id = req.session.user.userInfo.id
+
+
+  if (req.files) {
+    var ext = req.files.photourl.mimetype.split('/')[1]
+    console.log('Inside editProfile -->> ext', ext)
+
+    var photoFile = req.files.photourl
+    fileName = req.session.user.userInfo.id+"_"+req.session.user.userInfo.first_name+"_"+req.session.user.userInfo.last_name+"."+ext;
+    photoFile.mv(path.join(__dirname,'../public/images/')+fileName, function(err) {
+      if (err) {
+        return next(err)
       }
-    });
+    })
+  }
+
+  var userInfo = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    gitlink: req.body.gitlink,
+    linkedin: req.body.linkedin,
+    photourl: fileName,
+  }
+
+  user.update( id, userInfo, function (error, results) {
+    if (error) {
+      return res.render( 'pages/edit-profile', {error: error, userInfo: userInfo} )
+    }
+    // save req.body.git_repo
+    for( var val of req.body.git_repo ) {
+      gitrepo.insert(id, val, function(err, result) {
+        if (err) return next(err)
+      })
+    }
+
+    res.redirect('/')
+  })
 }
