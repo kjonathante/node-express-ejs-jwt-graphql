4346d7ca9da00191f7b04d349ccdc50848996581
Taylor Aanenson <tayloraanenson@Taylors-MacBook-Pro.local>
2018-09-05 17:54:08 -0700

    adjusted responsiveness

--- app/public/assets/javascript/profile.js
+++ app/public/assets/javascript/profile.js
@@ -2,6 +2,7 @@ $(document).ready ( function(){
     var path = window.location.href.split('/');
     var length = path.length;
     path = path[length-1];
-
+        
     console.log(path);
- })
+    $("#hiddenMessageInput").val(path);
+})
