7ffa9b523bcd12730f2d0b2b0bb49feaeaa9e9b0
Kit Jonathan Te <kjonathante@gmail.com>
2018-09-07 22:59:23 -0700

    modify ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,12 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-54-193-113-108.us-west-1.compute.amazonaws.com',
+      host: 'ec2-54-219-172-202.us-west-1.compute.amazonaws.com',
       key: '~/ec2key.pem',
       ref: 'origin/kt-deploy',
       repo: 'git@deployment:kjonathante/PRJ2-Express-Mysql.git',
       path: '/usr/local/workspace/PRJ2-Express-Mysql',
-      'post-deploy': 'yarn install && pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'yarn install && cp ~/.env /usr/local/workspace/PRJ2-Express-Mysql/. && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
